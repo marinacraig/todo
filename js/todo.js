8e55635a677379c1d6ee617a699bff852c55a14b
Marina <marina.enadeghe@gmail.com>
2018-02-22 23:16:20 +0100

    event listener für jeden eintrag

--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,7 @@
 
 /**
  * neue variable um auf todooo bzw. jene Listen _Klasse zuzugreifen
- * @type {TodoList}
+ *
  */
 let todoList = new TodoList();
 
@@ -11,20 +11,23 @@ ready(() => {
      * variable zum eingabe erkennbar machen sowie wohin es soll
      * @type {NodeListOf<Element>}
      */
-    let inputNewTask = document.querySelectorAll('.todos__new')
-    let todoList = document.getElementById('todos__list')
+    let inputNewTask = document.querySelectorAll('.todos__new');
+    let todoList = document.getElementById('todos__list');
 
-    inputNewTask.forEach ((input) => {
-        input.addEventListener ('keypress', e => {
-            if (e.keyCode === 13 && e.value !=='') {
-                /**
-                 * kreiire einen neuen Eintrag
-                 */
-                let task = todoList.addTask(input.value);
-                addTaskToList(task);
-                // input wieder leeren
-                input.value = '';
+    /**
+     * Loop für jede neue Eingabe im Eingabefeld
+     */
+    [].forEach.call(inputNewTask, function(event) {
+        /**
+         * für jeden eintrag darauf achten ob die Enter Taste gedrückt wurde und ob dieser nicht leer ist
+         */
+        event.addEventListener("keypress", function (e) {
+            if(e.keyCode === 13 && event.value !== '') {
+                console.log(event.value);
+
             }
         })
-    })
-})
+    });
+
+});
+
